badb97a95233485c13dbc0d6f95e22e07107604c
catamaro <anapamaro98@gmail.com>
2020-11-29 16:10:08 +0000

    project setup

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+#from sqlalchemy import SQLAlchemy
+#from flask_login import LoginManager
+#from flask_bootstrap import Bootstrap
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+
+from config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+#db = SQLAlchemy(app)
+
+#bootstrap = Bootstrap(app)
+
+fenix_blueprint = OAuth2ConsumerBlueprint(
+    "fenix-example", __name__,
+    # this value should be retrived from the FENIX OAuth page
+    client_id="1695915081466073",
+    # this value should be retrived from the FENIX OAuth page
+    client_secret="QRoQJy4t2uXFJgyV53X0P6ItK/5dXMXqbi9MS4C2n+Eas77cDvO8rjk5Pz3KezO9E6H2Ov1m/aHjqfUehQf9Mg==",
+    # do not change next lines
+    base_url="https://fenix.tecnico.ulisboa.pt/",
+    token_url="https://fenix.tecnico.ulisboa.pt/oauth/access_token",
+    authorization_url="https://fenix.tecnico.ulisboa.pt/oauth/userdialog",
+)
+
+app.register_blueprint(fenix_blueprint)
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from app import app, fenix_blueprint
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import jsonify, url_for
+from flask import session
+
+
+@app.route('/')
+def home_page():
+    # The access token is generated everytime the user authenticates into FENIX
+    print(fenix_blueprint.session.authorized)
+    print("Access token: "+ str(fenix_blueprint.session.access_token))
+    return render_template("appPage.html", loggedIn = fenix_blueprint.session.authorized)
+
+
+@app.route('/logout')
+def logout():
+    # this clears all server information about the access token of this connection
+    res = str(session.items())
+    print(res)
+    session.clear()
+    res = str(session.items())
+    print(res)
+    # when the browser is redirected to home page it is not logged in anymore
+    return redirect(url_for("home_page"))
+
+    
+
+@app.route('/private')
+def private_page():
+    #this page can only be accessed by a authenticated user
+
+    # verification of the user is  logged in
+    if fenix_blueprint.session.authorized == False:
+        #if not logged in browser is redirected to login page (in this case FENIX handled the login)
+        return redirect(url_for("fenix-example.login"))
+    else:
+        #if the user is authenticated then a request to FENIX is made
+        resp = fenix_blueprint.session.get("/api/fenix/v1/person/")
+        #res contains the responde made to /api/fenix/vi/person (information about current user)
+        data = resp.json() 
+        print(resp.json())
+        return render_template("privPage.html", username=data['username'], name=data['name'])
